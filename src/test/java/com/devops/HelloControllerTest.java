f1f9a618df8efe46f9f46332413dc24553dd1990
Vimal-P-3692 <vimalp3692@gmail.com>
2026-03-31 12:20:45 +0530

    first commit

---
+++ src/test/java/com/devops/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testHelloEndpoint() {
+        String response = this.restTemplate.getForObject(
+                "http://localhost:" + port + "/hello",
+                String.class
+        );
+
+        assertThat(response).contains("Hello World!");
+    }
+}
